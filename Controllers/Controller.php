c83a060df90ab3dc94b9f73f7d78696622214bdc
Stéphane Bouret <sbouret@orange.fr>
2026-01-20 19:09:09 +0100

    Templating MVC

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -6,12 +6,18 @@ namespace App\Controllers;
 
 abstract class Controller
 {
-    protected function render(string $path, array $data = []): void
+    protected function render(string $path, array $data = [], string $layout = 'layout/base'): void
     {
-        // Transforme les clés du tableau en variables
-        extract($data);
+        // 1) On transforme le tableau en variables
+        // EXTR_SKIP évite d'écraser des variables internes comme $content, $pageTitle...
+        extract($data, EXTR_SKIP);
         
-        // Inclut le fichier de la vue
+        // 2) Buffer de la vue : on capture le HTML produit par la vue
+        ob_start();
         require dirname(__DIR__) . "/Views/{$path}.php";
+        $content = ob_get_clean();
+        
+        // 3) Layout : il va utiliser $content (+ éventuellement $pageTitle, ...)
+        require dirname(__DIR__) . "/Views/{$layout}.php";
     }
 }
